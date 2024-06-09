a26471fdb38add3b0f3489749c2e25e31e7105a4
Shashi Sharma <sharmashashi940@gmail.com>
2024-06-09 11:11:37 +0545

    optimized formatter function

--- nvim/formatter/formatter.lua
+++ nvim/formatter/formatter.lua
@@ -1,26 +1,15 @@
--- Function to check if the current buffer's filetype is Dart
-local function isDartFile()
+local function getFileType()
 	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-	return filetype == "dart"
-end
-
-local function isGoFile()
-	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-	return filetype == "go"
-end
-
-local function isLuaFile()
-	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-	return filetype == "lua"
+	return filetype
 end
 
 -- command to save, format and re load the current dart file
 function FormatCurrentFile()
-	if isDartFile() then
+	if getFileType() == "dart" then
 		vim.cmd(":!dart format %")
-	elseif isGoFile() then
+	elseif getFileType() == "go" then
 		vim.cmd(":!gofmt %")
-	elseif isLuaFile then
+	elseif getFileType() == "lua" then
 		vim.cmd(":!stylua %")
 	else
 		vim.cmd(":!prettier % --write")
